a66e1b1b0bc27df763b7570798473b352c9f98e5
Minh Tran <trnmi822@student.otago.ac.nz>
2021-09-01 22:52:40 +1200

    Create javascript files for the tracking map

---
+++ scripts/map.js
@@ -0,0 +1,17 @@
+var Map = (function() {
+    var pub ={};
+
+    function onMapClick(e){
+
+    }
+
+    function centreMap(e){
+
+    }
+
+    pub.setup = function(){
+
+    };
+
+    return pub;
+}());
