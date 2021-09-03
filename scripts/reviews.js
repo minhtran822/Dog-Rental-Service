672daeaf603ca46e71e8e81f9a10048d49ec193c
Minh Tran <trnmi822@student.otago.ac.nz>
2021-09-03 18:58:22 +1200

    Show the reviews in index page

---
+++ scripts/reviews.js
@@ -0,0 +1,45 @@
+var Reviews = (function (){
+    var pub ={};
+
+    function getReviews(e){
+        console.log("Get Reviews called");
+        var target = $(".reviews")
+        var jsonSource = "../reviews/reviews.json";
+
+
+        $.ajax({
+            type: "GET",
+            url: jsonSource,
+            cache: false,
+            success: function(data) {
+                loadReviews(data, target);
+            },
+            error: function() {
+                alert("Can't connect to the json");
+            }
+        });
+    }
+
+    function loadReviews(data, target) {
+        var reviewKeys = ["title", "author", "reviewcontent"];
+        console.log("OK");
+        $.each(data, function (key, review){
+            if(reviewKeys.every(i => Object.keys(review).includes(i))){
+                var reviewDiv = document.createElement('div');
+                $(reviewDiv).addClass("review");
+                $(reviewDiv).append("<h3>"+review.title+"</h3>");
+                $(reviewDiv).append("<i>" +review.author+ "</i>");
+                $(reviewDiv).append("<p>" +review.reviewcontent+ "</p>");
+                $(target).append(reviewDiv);
+            }
+        });
+    }
+
+    pub.setup = function (){
+        getReviews();
+    };
+
+    return pub;
+}());
+
+$(document).ready(Reviews.setup);
