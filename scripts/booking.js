d9ab3de9e87ef23c850d1d348b3d9e7e25b32195
Minh Tran <trnmi822@student.otago.ac.nz>
2021-09-05 05:02:54 +1200

    Add current booking details

--- scripts/booking.js
+++ scripts/booking.js
@@ -15,7 +15,12 @@ var Booking = (function (){
       }
 
       if(items.length < 3){
-         items.push(JSON.stringify($(selectedDog).html()));
+         if(!items.includes(JSON.stringify($(selectedDog).html()))){
+
+            items.push(JSON.stringify($(selectedDog).html()));
+         } else {
+            alert("You have selected this dog");
+         }
       } else {
          alert("You have added the full limit of 3 dogs");
       }
@@ -23,9 +28,67 @@ var Booking = (function (){
       window.localStorage.setItem("currentBooking", JSON.stringify(items));
    }
 
-   pub.setup = function (){
+   function viewBooking(e){
+      $("#bookingDetails").append(
+          "<p>Pickup time: "+window.localStorage.getItem("time")+"</p>"+
+          "<p>Number of hours: "+window.localStorage.getItem("hours")+"</p>"+
+          "<p>Items: </p>"
+      );
 
+      var itemsDiv = document.createElement('div');
+      $(itemsDiv).addClass("items");
+
+      loadItems(itemsDiv);
+
+      $("#bookingDetails").append(itemsDiv);
+   }
+
+   function loadItems(itemsDiv){
+
+      var items = JSON.parse(window.localStorage.getItem("currentBooking"));
+      console.log(Array.isArray(items));
+      $(itemsDiv).empty();
+      if(Array.isArray(items)){
+         items.forEach(function(item){
+            console.log("ok");
+            $(itemsDiv).append("<p>"+item+"<input type='button' class='remove' value='remove'></p>");
+         });
+      }
+
+   }
+
+   function removeBooking(e){
+
+      var index = $(this).parent().index();
+      var items = JSON.parse(window.localStorage.getItem("currentBooking"));
+
+      items.splice(index,1);
+      window.localStorage.setItem("currentBooking", JSON.stringify(items));
+
+      location.reload();
+   }
+
+   function saveBooking(e){
+      var booking;
+      if($("bookingNameError").html()!== ""){
+         booking = {
+            items:  JSON.parse(window.localStorage.getItem("currentBooking")),
+            name: $("#bookingName").val(),
+            pickup: window.localStorage.getItem("time"),
+            numHours: window.localStorage.getItem("hours")
+         };
+
+         window.localStorage.clear();
+         window.localStorage.setItem("booking", JSON.stringify(booking));
+         alert("Saving Successful");
+      }
+   }
+
+   pub.setup = function (){
       $("main").on("click", ".chooseDog", addBooking);
+      $("#bookingDetails").onload = viewBooking();
+      $("#bookingDetails").on("click", ".remove", removeBooking);
+      $("#bookingInfo").submit(saveBooking);
    }
 
    return pub;
