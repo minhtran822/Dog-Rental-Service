07551caa53c5e20a428e21cec01d170a06e360a0
Minh Tran <trnmi822@student.otago.ac.nz>
2021-10-18 20:39:02 +1300

    Move authentication to protected directory

---
+++ functions/authFunction.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+require ('functions/dbconnect.php');
+
+$inName = htmlentities($_POST['loginUser']);
+$inPassword = htmlentities($_POST['loginPassword']);
+
+$query = "SELECT * FROM Users WHERE username = '".$inName."' AND password = SHA('".$inPassword."');";
+
+$result = $conn->query($query);
+if ($result->num_rows !== 0) {
+    unset($_SESSION['errorLogin']);
+    $_SESSION['username'] = $inName;
+} else {
+    $_SESSION['errorLogin'] = "The username and password does not exist.";
+}
+
+$result->free();
+$conn->close();
+?>
