e9f80c479ed6ac74f5310637c1209f828927a6ca
Minh Tran <trnmi822@student.otago.ac.nz>
2021-10-19 05:38:10 +1300

    Remove unnecessary functions

--- processBookingContent.php
+++ processBookingContent.php
@@ -6,25 +6,6 @@ $arr = json_decode(file_get_contents("php://input"));
 //Use this value for bookings details i.e: $val->numHours, pickup, items, name
 $val = json_decode($arr);
 
-$booking = simplexml_load_file('src/order.xml');
-$newBooking = $booking->addChild('booking');
-$newBooking->addChild('name', $val->name);
-$newBooking->addChild('numHours', $val->numHours);
-
-$pickupBooking = $newBooking->addChild('pickup');
-$pickupBooking->addChild('time', $val->pickup->time);
-$pickupBooking->addChild('day', $val->pickup->day);
-$pickupBooking->addChild('month', $val->pickup->month);
-$pickupBooking->addChild('year', $val->pickup->year);
-
-$items = $newBooking->addChild('items');
-$itemsArr = $val->items;
-foreach ($val->items as $item){
-    $items->addChild('item', $item);
-}
-
-$booking->saveXML('src/order.xml');
-
 
 $json_input = file_get_contents('src/bookings.json');
 $json_object = json_decode($json_input);
